06b2f35b5a4abc90e320748f22834c52a9381ad2
Stefan Hoelzl <stefan.hoelzl@posteo.de>
2018-10-01 23:38:40 +0200

    [internal] license, version and author information added

--- synopse/__init__.py
+++ synopse/__init__.py
@@ -1,5 +1,9 @@
 """synopse"""
 
+__author__ = "Stefan Hoelzl"
+__license__ = "MIT"
+__version__ = "0.1.0"
+
 from .blueprint import Blueprint
 from .structure import Structure
 from .errors import RequiredAttributeMissing, AttributeValidationFailed
