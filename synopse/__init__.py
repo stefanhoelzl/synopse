103fb27f2aaa415510bf8828c4f8fc8ad93b3c5b
Stefan Hoelzl <stefan.hoelzl@posteo.de>
2018-12-09 14:04:17 +0100

    [internal] added NativeComponent and CompositeComponent to init

--- synopse/__init__.py
+++ synopse/__init__.py
@@ -4,6 +4,10 @@ __author__ = "Stefan Hoelzl"
 __license__ = "MIT"
 __version__ = "0.1.0"
 
-from .component import Component
+
 from .errors import RequiredAttributeMissing, AttributeValidationFailed
 from .attributes import Attribute
+
+from .component import Component
+from .native_component import NativeComponent
+from .composite_component import CompositeComponent
