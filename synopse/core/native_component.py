ead81722a5b2944e95ad3cc9171b6039ff7d0f5a
Stefan Hoelzl <stefan.hoelzl@posteo.de>
2018-12-10 22:01:43 +0100

    [internal] deleted hooks to remove/insert in NativeComponent
    not needed for wx, can be added again if needed later

--- synopse/core/native_component.py
+++ synopse/core/native_component.py
@@ -17,41 +17,22 @@ def _flattened_layout(layout: Dict) \
 
 class NativeComponent(Component[Dict]):
     """Native Component"""
-    def insert(self, key: str, position: Optional[int],
-               component: "NativeComponent") -> None:
-        """TODO"""
-        raise NotImplementedError()
-
-    def remove(self, key: str, position: Optional[int],
-               component: "NativeComponent") -> None:
-        """TODO"""
-        raise NotImplementedError()
-
     def layout(self) -> Dict:
         """Layout is described my its attributes"""
         return self.attributes
 
     def mount(self, index: Optional[Index] = None) -> None:
         """Mounts itself and all its children
-
-        If an index is provided, the components inserts itself into the host.
         """
         super().mount(index)
-        if index:
-            index.host.insert(index.key, index.position, self)
         for key, pos, child in _flattened_layout(self.content):
             child.mount(Index(self, key, pos))
 
     def unmount(self) -> None:
         """Unmounts all children and itself.
-
-        If the component has an index,
-        the components removes itself from the host
         """
         for _, _, child in _flattened_layout(self.content):
             child.unmount()
-        if self.index:
-            self.index.host.remove(self.index.key, self.index.position, self)
         super().unmount()
 
     def update(self, attributes: Optional[Dict[str, Any]] = None) -> None:
--- tests/unit/core/test_native_component.py
+++ tests/unit/core/test_native_component.py
@@ -15,7 +15,6 @@ class TestNativeComponent:
 
     def test_mount_children(self):
         component = NativeComponent()
-        component.insert = MagicMock()
         component.layout = MagicMock(
             return_value={"k": NativeComponent(), "p": [NativeComponent()]}
         )
@@ -23,28 +22,13 @@ class TestNativeComponent:
         assert Index(component, "k", None) == component.content["k"].index
         assert Index(component, "p", 0) == component.content["p"][0].index
 
-    def test_insert_if_index_provided(self):
-        mock_host = MagicMock()
-        component = NativeComponent()
-        component.mount(Index(mock_host, "k", 1))
-        mock_host.insert.assert_called_once_with("k", 1, component)
-
     def test_unmount_children(self):
         component = NativeComponent()
-        component.remove = MagicMock()
         key_child, pos_child = MagicMock(), MagicMock()
         component.content = {"k": key_child, "p": [pos_child]}
         component.unmount()
         key_child.unmount.assert_called_once()
         pos_child.unmount.assert_called_once()
-
-    def test_remove_if_index_provided(self):
-        mock_host = MagicMock()
-        component = NativeComponent()
-        component.content = {}
-        component.index = Index(mock_host, "k", 1)
-        component.unmount()
-        mock_host.remove.assert_called_once_with("k", 1, component)
 
     def test_update_reconciles_content(self):
         component = NativeComponent()
